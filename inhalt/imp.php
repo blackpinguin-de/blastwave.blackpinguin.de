891e1e7e2ceeef72e802888807908e3d605f6fac
Robin C. Ladiges <rcl@blackpinguin.de>
2017-11-11 14:35:52 +0100

    rel=noopener

--- inhalt/imp.php
+++ inhalt/imp.php
@@ -1,5 +1,5 @@
 ﻿<?php $rcl->impressum(); ?>
 <br/>
 <div>
-Die Bilder sind geistiges Eigentum von <a href="http://www.blastwave-comic.com/" target="_blank">Kimmo Lemetti</a>
+Die Bilder sind geistiges Eigentum von <a href="http://www.blastwave-comic.com/" target="_blank" rel="noopener">Kimmo Lemetti</a>
 </div>
